f65cb09346f09e4441bb764320eb25838d298c0a
jhacsun <67787823+jhacsun@users.noreply.github.com>
2022-02-02 22:25:30 -0600

    reformatting text

--- features/reminder.js
+++ features/reminder.js
@@ -7,6 +7,6 @@ module.exports = () => {
 
     function remind() {
         const channel = bot.channels.cache.get("827374551708598302");
-        channel.send("⸝⸝ <@&772554025542484018>... ⊹˚.⋆ /n・✰﹕**HOW ARE YOU** /n> <:r_yes:827291601377296454> ⸝⸝ I’m doing good today /n> <:r_no:827388847330426880> ⸝⸝ I’m doing bad today");
+        channel.send("⸝⸝ <@&772554025542484018>... ⊹˚.⋆ /n・✰﹕**HOW ARE YOU** /n> <:r_yes:827291601377296454> ⸝⸝ I'm doing good today /n> <:r_no:827388847330426880> ⸝⸝ I'm doing bad today");
     }
 }
